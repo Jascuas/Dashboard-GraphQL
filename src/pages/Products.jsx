49dfdb1c1c313308992864da0eaca7ae27fc0346
Jascuas <loljavierdelago@hotmail.com>
2022-06-15 12:14:35 +0200

    working with graphql

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Sidebar from '../partials/Sidebar';
 import Header from '../partials/Header';
@@ -8,9 +8,68 @@ import ProductsTable from '../partials/products/ProductsTable';
 import PaginationNumeric from '../components/PaginationNumeric';
 
 function Products() {
-
+  const [data, setData] = useState(Object);
+  const apiEndpoint = "http://vps-123eb2fc.vps.ovh.net/graphql";
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const query = `
+  query FetchProducts(
+    $tax_filter: [String!],
+    $title_filter: String,
+    $order_by: String,
+    $order: String,
+    $page: Int!,
+    $per_page: Int!){
+    fetchProducts {
+    results(taxFilter: $tax_filter, titleFilter: $title_filter, orderBy: $order_by, order: $order, page: $page, perPage:
+    $per_page) {
+    id
+    title
+    price
+    tax
+    stock
+    }
+    pagination(taxFilter: $tax_filter, titleFilter: $title_filter, orderBy: $order_by, order: $order, page: $page, perPage:
+    $per_page) {
+    totalResults
+    limitValue
+    totalPages
+    currentPage
+    nextPage
+    prevPage
+    firstPage
+    lastPage
+    outOfRange
+    }
+    }
+    }
+`;
 
+  const getData = () => {
+    fetch(apiEndpoint, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        query,
+        variables: { 
+          // "tax_filter": ["es_general_21", "es_reduced_10"],
+          // "title_filter": "",
+          // "order_by": "price",
+          // "order": "desc",
+          "page": 1,
+          "per_page": 7
+           }
+      })
+    })
+      .then(res => res.json())
+      .then((data) => {
+        setData(data)
+      } )
+      .catch(console.error);
+  };
+  useEffect(() => {
+    getData()
+  }, [])
+  console.log(data)
   return (
     <div className="flex h-screen overflow-hidden">
 
